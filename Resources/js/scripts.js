132d14d2c81656deaf3b9680dbd650c893ef4ef9
0Lilymoon0 <thelilcinnabun1@gmail.com>
2020-09-21 23:12:43 -0700

    displayed cart items on console

--- Resources/js/scripts.js
+++ Resources/js/scripts.js
@@ -39,13 +39,48 @@ for (let i=0; i<data.length; ++i) {
 
 const cart = [ ]
 
+// Add Item
 function addItem(name, price) {
-    const item = {name: name, price: price, qty: 1}
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            cart[i].qty += 1
+            // Stop code
+            return
+        }
+    }
+      //name: name, price: price, qty: 1
+    const item = {name, price, qty: 1}
     cart.push(item)
 }
 
+// Show Items
 function showItems() {
-    console.log(`You have ${cart.length} items in your cart`)
+    
+    console.log(`You have ${getQty()} items in your cart`)
+
+    for (let i = 0; i < cart.length; i += 1) {
+        console.log(`- ${cart[i].name}: $${cart[i].price} x ${cart[i].qty}`)
+    }
+
+    console.log(`Total in cart: $${getTotal()}`)
+}
+
+// Get Qty
+function getQty() {
+    let qty = 0;
+    for (let i = 0; i < cart.length; i += 1) {
+        qty += cart[i].qty;
+    }
+    return qty
+}
+
+// Get Total
+function getTotal() {
+    let total = 0;
+    for (let i = 0; i < cart.length; i += 1) {
+        total += cart[i].price * cart[i].qty;
+    }
+    return total.toFixed(2);
 }
 
 addItem("happy", 5.99)
